21c65477457e5f9e04f359228ec74041f7322e96
Zoey1102 <s2258945@ed.ac.uk>
2021-11-14 05:02:05 +0000

    Update WG09P4.R
    Basic settings

--- WG09P4.R
+++ WG09P4.R
@@ -1,3 +1,20 @@
 # Group_09, Chang LU, Hongxuan ZHU, Zheyi SHEN
 # https://github.com/Zoey1102/BFGS_Optimizer
 
+bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
+  # A BFGS method optimiser.
+  # theta is a vector of initial values for the optimisation parameters.
+  # f is the objective function to minimise. Called as f(theta, gradientLogical,...) 
+  #   - theta is the vector of optimisation parameters.
+  #   - gradientLogical is a logical indicating if gradients of the objectives w.r.t the parameters should be computed.
+  #   - ... where all other arguments are passed
+  #   - if gradientLogical = TRUE, f will have an attribute "gradient" (vector)
+  # tol is the convergence tolerance.
+  # fscale is a tough estimate of the magnitude of f at the minimum. Used in convergence testing.
+  # maxit is the maximum number of BFGS iterations to try before giving up.
+  f0 <- f(theta,...)
+  
+  
+  
+  list(f=as.numeric(f0),theta=theta,iter=i,g=g,H=)
+}
