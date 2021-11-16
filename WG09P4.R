9af2e5756f28eb086e9d58b89b0ee5bdf2871a11
hzhu3835 <hzhu3835@uni.sydney.edu.au>
2021-11-16 15:30:01 +0000

    rename

--- WG09P4.R
+++ WG09P4.R
@@ -9,7 +9,7 @@ bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
   # set a tiny value 'hep' for figuring gradient
   hep = 1e-07
   # set an empty vector 'gr' to store the gradient value
-  gr = rep(0,length(theta))
+  g = rep(0,length(theta))
   # set a diagonal matrix as the initial approximate Hessian matrix
   bk = diag(length(theta))
   while(m <= maxit){
@@ -19,35 +19,35 @@ bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
       pos[i] = 1
       theta_add = theta + pos*hep
       theta_substract = theta - pos*hep
-      gr[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
+      g[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
     }
     # when the gradient value meet the request, loop end
-    if(sum(abs(gr)) < tol) break
+    if(sum(abs(g)) < tol) break
     # when fail to meet the request, figure out the step size 'a'
     else{
       # figure out update direction 'dk'
-      dk = -gr %*% solve(bk)
+      dk = -g %*% solve(bk)
       a = 10
       lameda = 0.8
       c = 0.5
       b = TRUE
       while(b){
-        if(fn(theta + a*dk)<fn(theta) + a * c * gr %*% t(dk)) b = FALSE
+        if(fn(theta + a*dk)<fn(theta) + a * c * g %*% t(dk)) b = FALSE
         else a = a * lameda
       }
       # renew theta
       theta_new = theta + a * dk
       # renew the approximate Hessian matrix
       sk = theta_new - theta_new
-      gr_new = rep(0,length(theta))
+      g_new = rep(0,length(theta))
       for(i in 1:length(theta)){
         pos = rep(0,length(theta))
         pos[i] = 1
         theta_add = theta_new + pos*hep
         theta_substract = theta_new - pos*hep
-        gr_new[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
+        g_new[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
       }
-      yk = gr_new - gr
+      yk = g_new - g
       if(yk %*% t(sk) > 0){
         bk = bk - (bk %*% t(sk) %*% sk %*% bk)/(sk %*% bk %*% t(sk)) +
           (t(yk) %*% yk)/(yk %*% t(sk))
@@ -58,6 +58,6 @@ bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
     m = m +1
   }
   f = fn(theta)
-  gr = gr_new
-  list(f = f,theta = theta,iter = m,g = gr,h = bk)
+  g = g_new
+  list(f = f,theta = theta,iter = m,g = g,h = bk)
 }
