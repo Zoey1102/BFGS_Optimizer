943ae8003fa68159f1d0879bc3f03419c5e9d17b
卢昶 <55506633+811202417@users.noreply.github.com>
2021-11-17 18:04:48 +0800

    Update WG09P4.R

--- WG09P4.R
+++ WG09P4.R
@@ -13,59 +13,63 @@ bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100) {
    return: a list of value at th minimum, including
            - objective function, parameters, iterations, gradients, approximate Hessian matrix"
   
-  fn <- function(theta) f(theta,...) ## return the function value
-  m = 1 ## number of iterations
-  hep = 1e-07 ## a tiny value for calculating gradient
-  g = rep(0,length(theta)) ## empty vector to store the gradient value
-  bk = diag(length(theta)) ## initialize approximate Hessian matrix as a diagonal matrix
-  
-  while (m <= maxit) {
+  # accept the arguments and return the function value
+  fn <- function(theta) f(theta,...)
+  # initiate the number of iterations
+  m = 1
+  # set a tiny value 'hep' for figuring gradient
+  hep = 1e-07
+  # set an empty vector 'gr' to store the gradient value
+  gr = rep(0,length(theta))
+  # set a diagonal matrix as the initial approximate Hessian matrix
+  bk = diag(length(theta))
+  while(m <= maxit){
     # calculate gradient value
-    for (i in 1:length(theta)) {
+    for(i in 1:length(theta)){
       pos = rep(0,length(theta))
       pos[i] = 1
       theta_add = theta + pos*hep
       theta_substract = theta - pos*hep
-      g[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
+      gr[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
     }
-    # when the gradient value meet the request, loop end
-    if (sum(abs(g)) < tol) break
     # when fail to meet the request, figure out the step size 'a'
-    else {
-      # figure out update direction 'dk'
-      dk = -g %*% solve(bk)
-      a = 10
-      lameda = 0.8
-      c = 0.5
-      b = TRUE
-      while (b) {
-        if (fn(theta + a*dk) < fn(theta) + a*c*g %*% t(dk)) b = FALSE
-        else a = a*lameda
-      }
-      # renew theta
-      theta_new = theta + a*dk
-      # renew the approximate Hessian matrix
-      sk = theta_new - theta_new
-      g_new = rep(0,length(theta))
-      for (i in 1:length(theta)) {
-        pos = rep(0,length(theta))
-        pos[i] = 1
-        theta_add = theta_new + pos*hep
-        theta_substract = theta_new - pos*hep
-        g_new[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
-      }
-      yk = g_new - g
-      if (yk %*% t(sk) > 0) {
-        bk = bk - 
-          (bk %*% t(sk) %*% sk %*% bk)/(sk %*% bk %*% t(sk)) +
-          (t(yk) %*% yk)/(yk %*% t(sk))
+    dk = -gr %*% solve(bk)
+    a = 10
+    lameda = 0.8
+    c = 0.5
+    b = TRUE
+    while(b){
+      if(fn(theta + a*dk)<fn(theta) + a * c * gr %*% t(dk)) b = FALSE
+      else a = a * lameda
+    }
+    # renew theta
+    theta_new = theta + a * dk
+    # renew the approximate Hessian matrix
+    sk = theta_new - theta
+    gr_new = rep(0,length(theta))
+    for(i in 1:length(theta)){
+      pos = rep(0,length(theta))
+      pos[i] = 1
+      theta_add = theta_new + pos*hep
+      theta_substract = theta_new - pos*hep
+      gr_new[i] = (fn(theta_add) - fn(theta_substract))/(2 * hep)
+    }
+    yk = gr_new - gr
+    # if sum(abs(yk)) < tol, which means this function converge, then loop break
+    if(sum(abs(yk)) < tol) break
+    # renew bk
+    else{
+      if(yk %*% t(sk) > 0){
+        bk = bk - (bk %*% t(sk) %*% sk %*% bk)/as.double(sk %*% bk %*% t(sk)) +
+          (t(t(yk)) %*% yk)/as.double(yk %*% t(sk))
       }
       else bk = bk
-      theta = theta_new
     }
-    m = m+1 ## count iterations
+    # renew theta,m
+    theta = theta_new
+    m = m +1
   }
+  gr = gr_new
   f = fn(theta)
-  g = g_new
-  list(f = f,theta = theta,iter = m,g = g,h = bk)
+  list(f = f,theta = theta,iter = m,g = gr,h = bk)
 }
